e01e0c96f696b22d816d8bced66b0b99c4fbdfd3
KazukiIMAI <kazuki.imai1118@gmail.com>
2025-12-11 20:50:00 +0900

    Day 1: 変数とf-stringの練習

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
-print("Hello, PFN! 私の挑戦が始まりました。")
+# 変数（箱）に値を入れる
+name = "PFNチャレンジャー"
+age = 23
+goal = "教育事業エンジニア"
+period = 3
+
+# 計算もできます（期間を日数に換算）
+days = period * 30
+
+# 結果を表示する（f-string という書き方です)
+print(f"私は{name}です。")
+print(f"{age}歳、未経験から{period}ヶ月(約{days}日)で{goal}になります！")
