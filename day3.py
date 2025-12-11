6d95b4edd5adedfe2996e57d14bdbcb98d7aec8e
KazukiIMAI <kazuki.imai1118@gmail.com>
2025-12-12 08:52:59 +0900

    Day 3: リストと辞書の学習

---
+++ day3.py
@@ -0,0 +1,9 @@
+# リストの例
+fruits = ["apple", "banana", "grape"]
+print(fruits[0])
+print(fruits[1])
+
+# 辞書の例
+profile = {"name": "Sato", "age": 25, "role": "Engineer"}
+print(f"私の名前は{profile['name']}です。")
+print(f"私の年齢は{profile['age']}です。")
